b2b998760eed662b61df52d24c2935331e6c967f
emilygmailprofissional-star <emilygmailprofissional@gmail.com>
2026-08-13 09:29:27 -0300

    13082026

--- br/com/Carro.java
+++ br/com/Carro.java
@@ -1,16 +1,7 @@
 package br.com;
 
 public class Carro {
-    private int velocidade;
+ public int velocidade;
     
-    public void setVelocidade(int v) {
-        if (v < 0) {
-            v = 0;
-        }
-        this.velocidade = v;
-    }
     
-    public int getVelocidade() {
-        return velocidade;
-    }
 }
--- br/com/Sistema.java
+++ br/com/Sistema.java
@@ -2,8 +2,7 @@ package br.com;
 
 public class Sistema {
     public static void main(String[] args) {
-        Carro meuCarro = new Carro();
-        meuCarro.setVelocidade(100);
-        System.out.println("Velocidade: " + meuCarro.getVelocidade());
+       Carro meuCarro = new Carro();
+        System.out.println("Velocidade: " + meuCarro);
     }
 }
