cd266f7720b45ffffee0349e28ca9a7e08b0c7a1
esc6880-lang <esc6880@gmail.com>
2026-08-18 23:02:11 -0300

    addação buzinar

---
+++ lista3/br/com/meusistema/model/Carro.java
@@ -0,0 +1,10 @@
+package br.com;
+package br.com.meusistema.model;
+public class Carro {
+    public String modelo;
+    public String cor;
+    public void buzinar (){
+        System.out.println("o"+modelo+" "+cor+"fez:BIIII BIII!");
+    }
+}
+
