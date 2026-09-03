984d5467d3b4de7eb5a9fd4d7e82b039ea23c19e
esc6880-lang <esc6880@gmail.com>
2026-09-03 08:50:23 -0300

    03092026

--- lista3/br/com/meusistema/model/Carro.java
+++ lista3/br/com/meusistema/model/Carro.java
@@ -7,4 +7,5 @@ public class Carro {
         System.out.println("o"+modelo+" "+cor+"fez:BIIII BIII!");
     }
 }
+//passo 3
 
--- lista3/br/com/src/br/com/meusistema/Sistema.java
+++ lista3/br/com/src/br/com/meusistema/Sistema.java
@@ -1,7 +1,9 @@
 package br.com;
+import  br.com.meusistema.model.Carro; 
 public class Sistema {
     public static void main(String[] args int) {
        Carro meuCarro = new Carro();
         System.out.println("Velocidade: " + meuCarro);
     }
 }
+//parte 4 
