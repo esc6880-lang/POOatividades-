86774399914ac2f452b8c70eeae8d93af4661017
esc6880-lang <esc6880@gmail.com>
2026-09-03 09:19:12 -0300

    add car

--- lista3/br/com/src/br/com/meusistema/Sistema.java
+++ lista3/br/com/src/br/com/meusistema/Sistema.java
@@ -1,9 +1,22 @@
 package br.com;
-import  br.com.meusistema.model.Carro; 
+import  br.com.meusistema.model.Carro; //citando a classe de outro bairo
+//parte 4 
+package br.com.meusistema.main;//bairo que classe sistema.java mora 
 public class Sistema {
-    public static void main(String[] args int) {
+    public static void main(String[] args) {
+        System.out.println("iniciando sistema ...");
+        //cria objeto
        Carro meuCarro = new Carro();
+       meuCarro.modelo="fusca_trubinado";
+       meuCarro.cor="roxo_uva";
+        Carro meuCarro2 = new Carro();
+       meuCarro2.modelo="fiat_uno";
+       meuCarro2.cor="azul_marinho";
+
+       //chamado de funçoes
         System.out.println("Velocidade: " + meuCarro);
+        meuCarro.buzinar();
+
     }
 }
-//parte 4 
+
